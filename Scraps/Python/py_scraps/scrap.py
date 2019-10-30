19733c5e579c3a5b918ea2086a38542252951980
RAJDEEP SINGH LATHER <rajdeepslather@gmail.com>
2019-10-30 01:03:08 -0400

    py_scraps

---
+++ Scraps/Python/py_scraps/scrap.py
@@ -0,0 +1,10 @@
+import math
+from sklearn.metrics.pairwise import cosine_similarity
+
+math.sqrt(1 + 2 ** 2 + 10 ** 2 + 5 ** 2 + 3 ** 2)
+
+d1 = [[10, 0, 0, 5, 0]]
+d2 = [[1, 4, 0, 5, 3]]
+d3 = [[0, 2, 10, 5, 3]]
+
+baby = [[1, 0, 0, 0, 0]]
