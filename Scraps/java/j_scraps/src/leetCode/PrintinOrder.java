b65a203b818d37ed1f2d1a91d135c018c618dcca
Rajdeep Singh Lather <rajdeepslather@gmail.com>
2019-11-26 02:51:08 -0500

    Fixed PrintInOrder

--- Scraps/java/j_scraps/src/leetCode/PrintinOrder.java
+++ Scraps/java/j_scraps/src/leetCode/PrintinOrder.java
@@ -1,28 +1,30 @@
 package leetCode;
 
+import java.util.concurrent.CountDownLatch;
+
 class PrintinOrder {
-    CountDownLatch firstLatch = new CountDownLatch(1);
-    CountDownLatch secondLatch = new CountDownLatch(1);
+	CountDownLatch firstLatch = new CountDownLatch(1);
+	CountDownLatch secondLatch = new CountDownLatch(1);
 
-    public PrintinOrder() {
-    }
+	public PrintinOrder() {
+	}
 
-    public void first(Runnable printFirst) throws InterruptedException {
-        // printFirst.run() outputs "first". Do not change or remove this line.
-        printFirst.run();
-        firstLatch.countDown();
-    }
+	public void first(final Runnable printFirst) throws InterruptedException {
+		// printFirst.run() outputs "first". Do not change or remove this line.
+		printFirst.run();
+		firstLatch.countDown();
+	}
 
-    public void second(Runnable printSecond) throws InterruptedException {
-        firstLatch.await();
-        // printSecond.run() outputs "second". Do not change or remove this line.
-        printSecond.run();
-        secondLatch.countDown();
-    }
+	public void second(final Runnable printSecond) throws InterruptedException {
+		firstLatch.await();
+		// printSecond.run() outputs "second". Do not change or remove this line.
+		printSecond.run();
+		secondLatch.countDown();
+	}
 
-    public void third(Runnable printThird) throws InterruptedException {
-        secondLatch.await();
-        // printThird.run() outputs "third". Do not change or remove this line.
-        printThird.run();
-    }
+	public void third(final Runnable printThird) throws InterruptedException {
+		secondLatch.await();
+		// printThird.run() outputs "third". Do not change or remove this line.
+		printThird.run();
+	}
 }
